28a3ef3cfe3bc4494b8ef0b7c72ddbc69870d7d4
pishadlaoslov <159408917+pishadlaoslov@users.noreply.github.com>
2025-10-20 15:51:59 +0500

    yerp

---
+++ Module7Lab3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IMediator
+{
+    void SendMessage(string message, Colleague colleague);
+}
+
+public abstract class Colleague
+{
+    protected IMediator _mediator;
+
+    public Colleague(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public abstract void ReceiveMessage(string message);
+}
+
+public class ChatMediator : IMediator
+{
+    private List<Colleague> _colleagues;
+
+    public ChatMediator()
+    {
+        _colleagues = new List<Colleague>();
+    }
+
+    public void RegisterColleague(Colleague colleague)
+    {
+        _colleagues.Add(colleague);
+    }
+
+    public void SendMessage(string message, Colleague sender)
+    {
+        foreach (var colleague in _colleagues)
+        {
+            if (colleague != sender)
+            {
+                colleague.ReceiveMessage(message);
+            }
+        }
+    }
+}
+
+public class User : Colleague
+{
+    private string _name;
+
+    public User(IMediator mediator, string name) : base(mediator)
+    {
+        _name = name;
+    }
+
+    public void Send(string message)
+    {
+        Console.WriteLine($"{_name} отправляет сообщение: {message}");
+        _mediator.SendMessage(message, this);
+    }
+
+    public override void ReceiveMessage(string message)
+    {
+        Console.WriteLine($"{_name} получил сообщение: {message}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ChatMediator chatMediator = new ChatMediator();
+
+        User user1 = new User(chatMediator, "Алиса");
+        User user2 = new User(chatMediator, "Боб");
+        User user3 = new User(chatMediator, "Чарли");
+
+        chatMediator.RegisterColleague(user1);
+        chatMediator.RegisterColleague(user2);
+        chatMediator.RegisterColleague(user3);
+
+        user1.Send("Привет всем!");
+        user2.Send("Привет, Алиса!");
+        user3.Send("Всем привет!");
+
+        Console.ReadLine();
+    }
+}
+
---
+++ Module7lab2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public abstract class Beverage
+{
+    public void PrepareRecipe()
+    {
+        BoilWater();
+        Brew();
+        PourInCup();
+        AddCondiments();
+    }
+
+    private void BoilWater()
+    {
+        Console.WriteLine("Кипячение воды...");
+    }
+
+    private void PourInCup()
+    {
+        Console.WriteLine("Наливание в чашку...");
+    }
+
+    protected abstract void Brew();
+    protected abstract void AddCondiments();
+}
+
+public class Tea : Beverage
+{
+    protected override void Brew()
+    {
+        Console.WriteLine("Заваривание чая...");
+    }
+
+    protected override void AddCondiments()
+    {
+        Console.WriteLine("Добавление лимона...");
+    }
+}
+
+public class Coffee : Beverage
+{
+    protected override void Brew()
+    {
+        Console.WriteLine("Заваривание кофе...");
+    }
+
+    protected override void AddCondiments()
+    {
+        Console.WriteLine("Добавление сахара и молока...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Beverage tea = new Tea();
+        Console.WriteLine("Приготовление чая:");
+        tea.PrepareRecipe();
+
+        Console.WriteLine();
+
+        Beverage coffee = new Coffee();
+        Console.WriteLine("Приготовление кофе:");
+        coffee.PrepareRecipe();
+
+        Console.ReadLine();
+    }
+}
